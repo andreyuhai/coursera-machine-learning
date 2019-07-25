67f0eb8924de06bf0fc3bad1c749fb49fb6f61d1
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-07-25 09:46:01 +0200

    Cost function for multiple features added, also with vectorized form

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,7 +13,11 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% Vectorized form:
+% A = sum((theta' .* X), 2) - y
+% J = (1 / (2 * m)) * (A' * A)
 
+J = (1 / (2 * m)) *  sum((sum((theta' .* X), 2) - y) .^ 2);
 
 
 

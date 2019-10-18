d13579d1f6d757622b0295aee98c5f3d276d65f4
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-10-18 20:33:35 +0300

    Regularization code has been completed

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -67,56 +67,75 @@ X = [ones(m, 1) X];
 
 % ---------------- Implementation with For Loop ----------------
 
-% for i = 1:m
-% 	% Calculate hypothesis, forward propagate
-% 	a1 = X(i, :)';
-% 	z2 = Theta1 * a1;
-% 	a2 = [1; sigmoid(z2)];
-% 	z3 = Theta2 * a2;
-% 	a3 = sigmoid(z3);
-% 	% Create a vector of zeros to vectorize our output
-% 	y_vectorized = zeros(num_labels, 1);
-% 	% Set corresponding index to 1, 0 should be indexed to 10 not to 1!
-% 	y_vectorized(y(i)) = 1;
-% 	% Sum all the output values from the output units and add it to J
-% 	J = J + (sum((- y_vectorized .* log(a3)) - ((1 - y_vectorized) .* log(1 - a3)))) / m
-% end
+for i = 1:m
+	% Calculate hypothesis, forward propagate
+	a1 = X(i, :)'; % 401x1 matrix
+	z2 = Theta1 * a1; % 25x1 matrix
+	a2 = [1; sigmoid(z2)]; % 26x1 matrix
+	z3 = Theta2 * a2; % 10x1 matrix
+	a3 = sigmoid(z3); % 10x1 matrix
+	% Create a vector of zeros to vectorize our output
+	y_vectorized = zeros(num_labels, 1);
+	% Set corresponding index to 1, 0 should be indexed to 10 not to 1!
+	y_vectorized(y(i)) = 1;
+	% Or just use yv = [1:num_labels] == y
+	% Sum all the output values from the output units and add it to J
+	J = J + (sum((- y_vectorized .* log(a3)) - ((1 - y_vectorized) .* log(1 - a3)))) / m;
+
+	delta_3 = a3 - y_vectorized; % 10x1 vector
+	delta_2 = (Theta2' * delta_3) .* sigmoidGradient([1; z2]); % 26x1 vector
+	% delta_2 = (Theta2' * delta_3) .* (a2 .* (1 - a2)); % Or you can use this one instead of using sigmoidGradient function
+	delta_2 = delta_2(2:end);
+
+	Theta1_grad = Theta1_grad + ((delta_2 * a1') ./ m);
+	Theta2_grad = Theta2_grad + ((delta_3 * a2') ./ m);
+end
+
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + ((lambda / m) * Theta1(:, 2:end));
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + ((lambda / m) * Theta2(:, 2:end));
 
 % ---------------- Vectorized Implementation ----------------
 
-v_z2 = Theta1 * X'; % 25x5000 matrix
-v_a2 = [ones(m, 1) sigmoid(v_z2')]; % 5000x26 matrix
-v_a3 = sigmoid(Theta2 * v_a2')'; % 5000x10 matrix
-eye_matrix = eye(num_labels);
-y_matrix = eye_matrix(y, :); % 5000x10 matrix
+% v_z2 = Theta1 * X'; % 25x5000 matrix
+% v_a2 = [ones(m, 1) sigmoid(v_z2')]; % 5000x26 matrix
+% v_z3 = (Theta2 * v_a2')'; % 5000x10 matrix
+% v_a3 = sigmoid(v_z3); % 5000x10 matrix
+% eye_matrix = eye(num_labels);
+% y_matrix = eye_matrix(y, :); % 5000x10 matrix
+
+
+% Gives all the outputs (h(x)) for all examples in the training set X
+
+% all_example_outputs = - y_matrix .* log(v_a3) - ((1 - y_matrix) .* log(1 - v_a3));
 
 % Below we get a 5000x10 matrix as a result. First we sum row-wise.
 % Then we get a 5000x1 matrix which we later sum column vise.
 % Divided by m we get the cost.
-J = sum(sum(-(y_matrix .* log(v_a3)) - ((1 - y_matrix) .* log(1 - v_a3)))) / m;
+
+% J = sum(sum(all_example_outputs)) / m;
+
+% unregularized_J = J;
+
+% ---------------- Regularization ----------------
+
+squared_Theta1 = Theta1 .^ 2;
+squared_Theta2 = Theta2 .^ 2;
+
+J = J + (sum(sum(squared_Theta1(:, 2:end))) + sum(sum(squared_Theta2(:, 2:end)))) * (lambda / (2 * m)); 
+
+% ---------------- Back Propagation ----------------
 
 
+% ---------------- Vectorized ----------------
 
+% delta_3 = v_a3 - y_matrix; % 5000x10 matrix
+% delta_2 = delta_3 * Theta2 .* (v_a2 .* (1 - v_a2)) % 5000x26 matrix
+% delta_2 = delta_2(:, 2:end);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% D2 = v_a2' * delta_3;
+% Theta2_grad = D2;
+% D1 = sum(sum(v_a1' * delta_2));
+% Theta1_grad = D1;
 
 % -------------------------------------------------------------
 
